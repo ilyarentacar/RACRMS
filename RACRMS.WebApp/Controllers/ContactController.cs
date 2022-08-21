56fee63181af36d318218308a8098811c344ad0a
rekasparov <mburakkayabal@hotmail.com>
2022-08-21 21:18:49 +0300

    wake-up service added.

---
+++ RACRMS.WebApp/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RACRMS.WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                if (HttpContext.Session.Keys.Any(x => x == "ErrorMessage"))
+                {
+                    ViewBag.ErrorMessage = HttpContext.Session.GetString("ErrorMessage");
+                    HttpContext.Session.Remove("ErrorMessage");
+                }
+
+                if (HttpContext.Session.Keys.Any(x => x == "SuccessMessage"))
+                {
+                    ViewBag.SuccessMessage = HttpContext.Session.GetString("SuccessMessage");
+                    HttpContext.Session.Remove("SuccessMessage");
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+    }
+}
